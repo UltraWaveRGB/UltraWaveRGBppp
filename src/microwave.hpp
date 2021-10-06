8cbe288af5939ce268fbeb21e281fc2a0d2f8873
REIS0 <aor@cesar.school>
2021-10-06 19:09:37 -0300

    Use only english

--- src/microwave.hpp
+++ src/microwave.hpp
@@ -1,11 +1,11 @@
 // TODO: checar
 #define OFF 0
 #define ON 1
-#define FECHADA 0
-#define ABERTA 1
+#define CLOSED 0
+#define OPEN 1
 
 void spin();
 void start();
 void stop();
-void abre_porta();
-void fecha_porta();
+void open_door();
+void close_door();
