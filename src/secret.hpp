58fde7f3b02c20cae15d63880d4a3c8d7a819d39
acrs <arthcrs@gmail.com>
2021-10-11 18:01:43 -0300

    Added secret.hpp

---
+++ src/secret.hpp
@@ -0,0 +1,8 @@
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+/* 2. If work with RTDB, define the RTDB URL and database secret */
+// <databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_URL "" 
+#define DATABASE_SECRET ""
